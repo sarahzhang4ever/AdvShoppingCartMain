dafda565218cd3d3d3a0223a0198564b8861e6af
zhf <hfloodnew0522@gmail.com>
2022-03-02 14:12:37 -0800

    Modify unit test format

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -4,10 +4,24 @@ import adshopcart_locators as locators
 
 
 class AdShopCartAppPositiveTestCases(unittest.TestCase):
-    @staticmethod
-    def test_case_a_homepage_functionality():
+
+    @classmethod
+    def setUpClass(cls):
         methods.setUp()
 
+    @classmethod
+    def tearDownClass(cls):
+        methods.tearDown()
+
+    def setUp(self):
+        print('\n===== Start of test:', self.shortDescription())
+
+    def tearDown(self):
+        print('===== End of test:', self.shortDescription())
+
+    @staticmethod
+    def test_case_a_homepage_functionality():
+        """Home Page Functionality"""
         methods.check_categories()
         methods.check_top_navi_menu()
         methods.check_main_logo()
@@ -15,6 +29,7 @@ class AdShopCartAppPositiveTestCases(unittest.TestCase):
 
     @staticmethod
     def test_case_b_register_sign_in_out_delete():
+        """Register-Sign-in-Sign-out-Delete"""
         methods.log_fake_data()
         methods.register_new_account()
         methods.check_new_user_info()
@@ -22,6 +37,4 @@ class AdShopCartAppPositiveTestCases(unittest.TestCase):
         methods.sign_in_with_right_credentials(locators.user_name, locators.password)
         methods.delete_account()
         methods.sign_in_with_wrong_credentials(locators.user_name, locators.password)
-
-        methods.tearDown()
 
